f79414b61af5da79812225e742533e11e213a3db
TaylorWhitt <Taylor.Whitt@gmail.com>
2015-11-15 12:26:09 -0800

    Update FindMTU.ps1
    First iteration, first useful script ever written.  I have not taken any classes or training on programming software, and there are already known bugs. Suggestions welcome!

---
+++ FindMTU.ps1
@@ -0,0 +1,62 @@
+# PShellNetwork
+# Powershell Scripts to assist with networking optomizations.
+# This is my very first scripting experience that actually does something useful - go easy on me/suggestions welcome.
+# There are a few bugs and future improvements annotated within the script.
+
+
+# Define parameters (future - selectable/prompt with defaults, logarithmic scaling starting at 0)
+# Future/side project, tracreroute and perform on a router basis to detect which router is causing overhead.  (useful in enterprise enviornment)
+
+$IPAddress = "www.google.com"
+$MaxMTU = 1536
+$MinMTU = 1000
+$MTUSize = 1001
+$MTUMaxFound = $false
+
+# Start low, go high.  Successful pings are quicker than no reply caused by proxy or like devices.
+while (-not $MTUMaxFound) {
+    
+    ping -f $IPAddress -l $MTUSize -n 1
+    
+    if ($LASTEXITCODE -eq 0) {
+        $MTUSize += 5        
+    } else {
+        Write-Host "Ping was fragmented"
+        $MTUMaxFound = $true
+    }
+    
+        if ($MTUSize -gt $MaxMTU) {
+        Write-Host "MTU is above threshold.  Congratulations, your network can support Jumbo packets end-to-end!"
+        $MTUMaxFound = $true
+        # (Future) Skip to optimized starting location for finding scaled MTU for Jumbo frames.
+    }
+
+}
+# Need retry with counter (or error code specificity) to account for packet-loss possibility (future)
+
+$MTUSizeFound = $false
+
+while (-not $MTUSizeFound) {
+    
+    ping -f $IPAddress -l $MTUSize -n 1
+    
+    if ($LASTEXITCODE -ne 0) {
+        $MTUSize -= 1
+
+    } else {
+    
+        Write-Host "Ping was fragmented"
+        $MTUSizeFound = $true
+    }
+    
+        if ($MTUSize -lt $MinMTU) {
+        Write-Host "MTU is below threshold or something is blocking the PING protocol."
+        $MTUSizeFound = $true
+    }
+}
+
+Write-Host "MTU Size: $MTUSize"
+
+# (future) Set NIC MTU size with commands below
+# netsh interface ipv4 show interfaces
+# netsh interface ipv4 set interface [interface Idx] mtu $MTUSize
