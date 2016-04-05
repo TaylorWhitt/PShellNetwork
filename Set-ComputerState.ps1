b66f37aaf72d891f4da8d0adf83765cae9896bea
Taylor Whitt <Taylor.Whitt@gmail.com>
2016-04-05 15:50:33 -0700

    Update Set-ComputerState.ps1
    Forgot to add back in the actual action of "win32shutdown($State)" after testing credential errors.

--- Set-ComputerState.ps1
+++ Set-ComputerState.ps1
@@ -4,12 +4,13 @@ Function Set-ComputerState {
 		Will log off, shutdown, reboot a local or remote computer.
 	.Description
 		Will log off, shutdown, reboot a local or remote computer.
-		"-Force" may return unexpected results, but will not require user interaction to close programs.
-		Credentials can be provided with a global $PSCredential variable, or can be prompted for with the -Credential switch parameter.  
+		"-Force" may return unexpected results, but will not require user interaction to close programs. They will lose unsaved work.
+		Credentials can be provided with a global $PSCredential variable (must be defined) or, 
+		With the -Credential switch parameter, it will prompt you for input.  
 		Otherwise, it will use the current session's credentials.
 	.Example
 	 Set-ComputerState computer.domain -LogOff
-        Will gracefully log off users on computer listed.
+        Will prompt logged-in users to log off on computer(s) listed, using current session credentials.
 #>
 	[CmdletBinding()]
     	Param(
@@ -17,7 +18,8 @@ Function Set-ComputerState {
         	Mandatory = $True,
 	        ValueFromPipeline = $True,
         	ValueFromPipelineByPropertyName = $True,
-          Position = 1)]
+          	Position = 1)]
+          	
 		[string[]]$ComputerName,
 		[switch]$Force,
 		[switch]$Credential,
@@ -47,11 +49,11 @@ Function Set-ComputerState {
 	    if ($ShutDown) 	{$State = 1}
 	    if ($Reboot)	{$State = 2}
 	    if ($PowerOff)	{$State = 8}
-	    if ($Force)     {$State += 4}
+	    if ($Force)     	{$State += 4}
             
       if ($Credential -and $PSCredential -ne $Null) {
         write-Host 'Using $PSCredential'
-        (Get-WmiObject win32_operatingsystem -ComputerName $Computer -ErrorAction SilentlyContinue -Credential $PSCredential)
+        (Get-WmiObject win32_operatingsystem -ComputerName $Computer -ErrorAction SilentlyContinue -Credential $PSCredential).win32shutdown($State)
         Trap [System.UnauthorizedAccessException] { 
           Write-Host "$Computer was provided with incorrect or insufficient credentials."
           continue
@@ -59,7 +61,7 @@ Function Set-ComputerState {
 
       } elseif ($Global:PSCredential -ne $Null) {
         write-Host 'Using $Global:PSCredential'
-        (Get-WmiObject win32_operatingsystem -ComputerName $Computer -ErrorAction SilentlyContinue -Credential $Global:PSCredential)
+        (Get-WmiObject win32_operatingsystem -ComputerName $Computer -ErrorAction SilentlyContinue -Credential $Global:PSCredential).win32shutdown($State)
         Trap [System.UnauthorizedAccessException] { 
           Write-Host "$Computer was supplied with incorrect or insufficient Global: credentials."
           continue
@@ -67,7 +69,7 @@ Function Set-ComputerState {
       
       } Else {
         Try {
-    	    (Get-WmiObject win32_operatingsystem -ComputerName $Computer -ErrorAction SilentlyContinue)
+    	    (Get-WmiObject win32_operatingsystem -ComputerName $Computer -ErrorAction SilentlyContinue).win32shutdown($State)
         } Catch [System.UnauthorizedAccessException] { 
             Write-Host "$Computer was supplied with insufficient session credentials."
         } #Try/Catch
@@ -75,7 +77,7 @@ Function Set-ComputerState {
 
     } # ForEach
 
-	} #PROCESS
+} #PROCESS
 
 	END {}
 
