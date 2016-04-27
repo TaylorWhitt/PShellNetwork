ee833a578e8cc6d0984326fc668728d6b63a6744
Taylor Whitt <Taylor.Whitt@gmail.com>
2016-04-26 17:58:43 -0700

    Update FindMTU.ps1
    Completely over-hauled with new knowledge of PowerShell.  Added basic help, optimized ping MTU increment, and other changes.  Oddly, if pasting directly into PowerShell, the netsh commands do not work and it tells me there are too many "}".  Works as expected in ISE.
    
    Still need error handling, and should detect if the MTU settings are too low prior to starting.  Unfortunately I am unfamiliar with how to access the NIC parameters directly and am unskilled in regex to pull the data from netsh commands.

--- FindMTU.ps1
+++ FindMTU.ps1
@@ -1,70 +1,160 @@
 # PShellNetwork
 # Powershell Scripts to assist with networking optomizations.
 # This is my very first scripting experience that actually does something useful - go easy on me/suggestions welcome.
-# There are a few bugs and future improvements annotated within the script.
+# I still need error handling and detection for low MTU settings.
 
-
-# Define parameters (future - selectable/prompt with defaults, logarithmic scaling starting at 0)
-# Future/side project, tracreroute and perform on per-hop basis to detect which network segments are causing overhead.  
-# (useful in an enterprise enviornment)
-
-$IPAddress = "www.google.com"
-$MaxMTU = 1536
-$MinMTU = 1000
-$MTUSize = 1001
-$MTUMaxFound = $false
-
-# Start low, go high.  Successful pings are quicker than no reply caused by proxy or like devices.
-while (-not $MTUMaxFound) {
+Function Find-MTU {
     
-    ping -f $IPAddress -l $MTUSize -n 1
+    <#
+    .Synopsis
+		Will conduct a series of ping requests to a target host with DF bit set.
+	.Description
+		Will conduct a series of ping requests to a target host with DF bit set.
+
+		-Set 
+Will perform netsh commands and prompt the user to change the interface specified to optimal settings.
+*Requires elevated privledges*
+
+  -ShowSteps 
+  Will write messages to the console to walk the user through the steps being performed within the function.
+	.Example
+	 Find-MTU www.google.com
+        Will conduct a series of Do-Not-Fragment ping requests to "www.google.com" until a Maximum Transmission Unit is found, and output 
+the result to the host. This does not account for the additional frame header which is an additional 24 or 28 bits depending on VLAN settings.
+     .Example
+     Find-MTU 8.8.8.8 -Set -ShowSteps
+Will conduct a series of Do-Not-Fragment ping requests target 8.8.8.8 until a Maximum Transmission Unit is found.
+Once found, will promt the user to select which interface is being used and will change the interface's MTU settings accordingly.
+It will account for the additional frame header and detects for VLAN settings. Each step will be written to the console as a Gee-Wiz.
+
+**NOTE** "-Set" requires administrator privledges to implement network interface changes.
+#>
     
-    if ($LASTEXITCODE -eq 0) {
-        $MTUSize += 5        
-    } else {
-        Write-Host "Ping was fragmented"
-        $MTUMaxFound = $true
-    }
+    Param(
+        [Parameter(Mandatory=$True,
+                   Position=1,
+                   HelpMessage='Server to ping')]
+        [string]$Address,
+        [switch]$Set,
+        [switch]$ShowSteps
+    ) #Param
+
+    BEGIN {}
+
+    PROCESS {
+
+    [int]$MaxMTU = 1536
+    [int]$MinMTU = $Null
+    [int]$MTUSize = $Null
+    [bool]$MTUMaxFound = $false
+    [int]$Difference = [math]::Truncate(($MaxMTU - $MinMTU) / 2)
+    [int]$Numberofsteps = 0
+    [int]$Count = 0
+
+    while ($Difference -gt 1) {
+        $Numberofsteps ++
+        if ($Showsteps) {
+        Write-Host "Trying Size: $MTUSize
+        "
+        Write-Host "Incrementing steps: $Numberofsteps"
+        }
+
+        ping -f $Address -l $MTUSize -n 1 | out-null
+        
     
-        if ($MTUSize -gt $MaxMTU) {
-        Write-Host "MTU is above threshold.  Congratulations, your network can support Jumbo packets end-to-end!"
-        $MTUMaxFound = $true
-        # (Future) Skip to optimized starting location for finding scaled MTU for Jumbo frames.
-    }
+            if ($LASTEXITCODE -eq 0) {
+                if ($ShowSteps) {
+                    Write-Host "Splitting difference (round down): $Difference"
+                } #If
+                $MTUSize += $Difference
+                $Difference = [math]::Truncate($Difference / 2 )        
+            
+            if ($ShowSteps) {
+                    Write-Host "Difference: $Difference"
+            } #If
 
-}
-# Need retry with counter (or error code specificity) to account for packet-loss possibility (future)
+            } else {
+                if ($ShowSteps) {
+                    Write-Host "Splitting difference: $Difference"
+                } #If
+                $MTUSize -= $Difference
+                $Difference = [math]::Truncate($Difference / 2 )
 
-$MTUSizeFound = $false
-
-while (-not $MTUSizeFound) {
+            } #If/Else
     
-    ping -f $IPAddress -l $MTUSize -n 1
-    
-    if ($LASTEXITCODE -ne 0) {
-        $MTUSize -= 1
+        } #While
 
-    } else {
-    
-        Write-Host "Ping was fragmented"
-        $MTUSizeFound = $true
-    }
-    
-        if ($MTUSize -lt $MinMTU) {
-        Write-Host "MTU is below threshold or something is blocking the PING protocol."
-        $MTUSizeFound = $true
-    }
-}
+    while ($Difference -eq 1 -and $Count -ne 2) {
 
-# Add 28 bits for frame header that is not included in ping size (payload) parameters.  (Payload+Header=MTU)
-# Need VLAN detection for optional 4 bits in header.
+        ping -f $Address -l $MTUSize -n 1 | out-null
+        $Numberofsteps ++
+        if ($ShowSteps) {
+            Write-Host "Incrementing steps: $Numberofsteps"
+        } #If
 
-$MTUSize=$MTUSize+28
-Write-Host "MTU Size: $MTUSize"
+            if ($LASTEXITCODE -eq 0) {
+                
+            $MTUSize ++
+            $Count ++
+                if ($ShowSteps) {
+                    Write-Host "Incrementing MTUSize: $MTUSize
+                    Incrementing Count: $Count"
+                } #If
 
-# Change internet adapter MTU settings.
+            } else { 
 
-netsh interface ipv4 show interfaces
-$Interface = Read-Host -Prompt 'Input which interface (Idx number) you would like to change'
-netsh interface ipv4 set interface "$Interface" "mtu=$MTUSize"
-netsh interface ipv4 show interfaces
+            $MTUSize -= 2 
+            $Count = 0
+            if ($ShowSteps) {
+                Write-Host "Incrementing MTUSize: $MTUSize
+                Failure, Count=$Count"
+                } #If
+            } #If/Else
+
+        } #While
+
+    $MTUSize -= 1
+
+    if ($ShowSteps) {
+        Write-Host "Incrementing MTUSize: $MTUSize"
+    } #If
+    Write-Host "Optimal MTU size of $MTUSize was found in $Numberofsteps steps."
+        
+    # Change internet adapter MTU settings.
+        
+    if ($Set) {
+
+    $24MTUSize = $MTUSize + 24  # Add 24 bits for frame header that is not included in ping size (payload) parameters.  (Payload+Header=MTU)
+    $28MTUSize = $MTUSize + 28  # If the computer uses a VLAN, add 4 extra bits to frame header.
+
+    netsh interface ipv4 show interfaces
+
+
+    $Interface = Read-Host 'Input which interface (Idx number) you would like to change'
+    if ($ShowSteps) {
+                Write-Host "Setting Local interface to: $24MTUSize to account for frame header"
+    } #If
+    Try {netsh interface ipv4 set interface "$Interface" "mtu=$24MTUSize"} #Try
+    catch {} #Catch
+        
+    ping -f $Address -l $MTUSize -n 1 | out-null | Wait-Event
+
+        if ($LASTEXITCODE -ne 0) {
+            if ($ShowSteps) {
+                Write-Host "VLAN detected, setting MTU to: $28MTUSize"
+            } #If
+        netsh interface ipv4 set interface "$Interface" "mtu=$28MTUSize"
+        } #If
+    Write-Host '
+    Current Configuration:
+    '
+    netsh interface ipv4 show interfaces
+
+
+    } #If
+
+    } #Process
+
+    END {}
+
+} #Function
