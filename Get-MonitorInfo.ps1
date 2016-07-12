22682d2fde2701b584296bc39f39e16a65757eb2
Taylor Whitt <Taylor.Whitt@gmail.com>
2016-07-12 11:04:53 -0700

    Update Get-MonitorInfo.ps1
    Put properties in BEGIN block as properties are not a parameter.

--- Get-MonitorInfo.ps1
+++ Get-MonitorInfo.ps1
@@ -1,46 +1,47 @@
-﻿function Get-MonitorInfo {
+function Get-MonitorInfo {
     param (
-        [Parameter(ValueFromPipeline=$true)]
-        $Properties = @(
-  @{
-    name       = 'Model'
-    expression = {
-      [System.Text.Encoding]::ASCII.GetString($_.UserFriendlyName)
-    }
-  }, 
-  @{
-    name       = 'SerialNumber'
-    expression = {
-      [System.Text.Encoding]::ASCII.GetString($_.SerialNumberID)
-    }
-  },
-    @{
-    name       = 'ProductCodeID'
-        expression = {
-          [System.Text.Encoding]::ASCII.GetString($_.ProductCodeID)
-        }
-  },
-    @{
-    name       = 'ManufacturerName'
-        expression = {
-          [System.Text.Encoding]::ASCII.GetString($_.ManufacturerName)
-        }
-  },
-  @{
-    name       = 'ComputerName'
-        expression = {$_.PSComputerName}
-  }
-), 
-        [string[]]$ComputerName = $env:COMPUTERNAME
+    [string[]]$ComputerName = $env:COMPUTERNAME
     )
-
-    BEGIN{}
-    PROCESS{
+    BEGIN {
+    $Properties = @(
+        @{
+            name       = 'Model'
+            expression = {
+            [System.Text.Encoding]::ASCII.GetString($_.UserFriendlyName)
+            }
+        } 
+        @{
+            name       = 'SerialNumber'
+            expression = {
+            [System.Text.Encoding]::ASCII.GetString($_.SerialNumberID)
+            }
+        }
+        @{
+            name       = 'ProductCodeID'
+            expression = {
+            [System.Text.Encoding]::ASCII.GetString($_.ProductCodeID)
+            }
+        }
+        @{
+            name        = 'ManufacturerName'
+            expression  = {
+            [System.Text.Encoding]::ASCII.GetString($_.ManufacturerName)
+            }
+        },
+        @{
+            name        = 'ComputerName'
+            expression  = {
+            $_.PSComputername
+            }
+        }
+        )
+      }
+    PROCESS {
         ForEach ($Computer in $ComputerName) {
             Get-WmiObject -ComputerName $Computer -Namespace 'root/WMI' -Class WMIMonitorID | 
             Where-Object -Property UserFriendlyName | 
             Select-Object -Property $Properties
         }
     }
-    END{}
+    END {}
 }
