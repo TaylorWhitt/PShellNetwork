60832220928d5db3b46a0ef32533d2bfdce508f8
Taylor Whitt <Taylor.Whitt@gmail.com>
2016-07-19 11:01:32 -0700

    Update Get-MTU.ps1
    Updated with .NET ping requests.  
    Added simple failure retry if empty ping failed too. 
    Changed to [math]::Ceiling instead of trunicate to prevent the rounding down at 3 - eliminated bug where it would return a value 1 less of possible value.
    Changed -showsteps to -Verbose.
    Changed to output the MTU value if -Set not specified (so it does what the script says it does).
    Various comment/grammar changes.

--- Get-MTU.ps1
+++ Get-MTU.ps1
@@ -9,14 +9,11 @@ Function Get-MTU {
 	Will conduct a series of ping requests to a target host with DF bit set.
     .Description
 	Will conduct a series of ping requests to a target host with DF bit set.
-
     -Set 
     Will perform netsh commands and prompt the user to change the interface specified to optimal settings.
     *Requires elevated privledges*
-
     -ShowSteps 
     Will write messages to the console to walk the user through the steps being performed within the function.
-
     -Idx
     Identifies the local interface to change within the -Set switch. If left blank, you will be prompted with your options.
 	
@@ -29,7 +26,6 @@ the result to the host. This does not account for the additional frame header wh
 Will conduct a series of Do-Not-Fragment ping requests target 8.8.8.8 until a Maximum Transmission Unit is found.
 Once found, will promt the user to select which interface is being used and will change the interface's MTU settings accordingly.
 This will account for the additional frame header and detects VLAN settings. Each step will be written to the console.
-
 **NOTE** "-Set" requires administrator privledges to implement network interface changes.
 #>
     
@@ -46,151 +42,119 @@ This will account for the additional frame header and detects VLAN settings. Eac
                    ParameterSetName='Set',
                    HelpMessage='Toggle to change the interface')]
         [switch]$Set,
-        [switch]$ShowSteps
+        [int]$Timeout = "500",
+        [int]$MaxMTU = "35840"
     ) #Param
 
-    BEGIN {}
+    BEGIN {
+    $PingObject = New-Object System.Net.NetworkInformation.Ping
+    $PingOptions = New-Object System.Net.NetworkInformation.PingOptions
+    [byte[]]$ByteBuffer = ,0xAC * $MaxMTU
+    [void][int]$MTUSize
+    [void][int]$Difference 
+    [int]$Difference = [math]::Abs($MaxMTU)
+    }
 
     PROCESS {
 
-    [int]$MaxMTU = 1500
-    [int]$MTUSize = $Null
-    [int]$Difference = [math]::Truncate(($MaxMTU - $MinMTU) / 2)
-    [int]$Numberofsteps = 0
-    [int]$Count = 0
+    
 
-        & PING.EXE $Address -n 2 | Out-Null
-        if ($LASTEXITCODE -ne 0) {
-    Write-Host "The address specified was not reachable. Please provide a reachable hostname or IP address, or try again.
-You Provided: $Address"
-    Break
+
+
+
+        Write-Verbose "Testing connection to $Address..."
+        $TestPing = $PingObject.Send($Address,$Timeout,$($ByteBuffer[1]))
+        if ($TestPing.status.ToString() -ne 'Success') {
+            $TestPing.status.ToString()
+            Write-Host "The address specified was not reachable. Please provide a reachable hostname or IP address, or try again. `nYou Provided: $Address"
+            Break
         } #If
 
         if ($Set) {
 
+            if ($Idx -ne $Null) {
+                $Interface = $Idx
+            } else {
+                & netsh.exe interface ipv4 show interfaces |Out-Null
+                $Interface = Read-Host 'Input which interface (Idx number) you would like to change'
+            }
 
-
-        if ($Idx -ne $Null) {
-        $Interface = $Idx
-        } else {
-        & netsh.exe interface ipv4 show interfaces
-        $Interface = Read-Host 'Input which interface (Idx number) you would like to change'
-        }
-
-        & netsh.exe interface ipv4 set interface "$Interface" "mtu=$MaxMTU"
+        & netsh.exe interface ipv4 set interface "interface=$Interface" "mtu=$MaxMTU"
             if ($LASTEXITCODE -ne 0) {
-            Write-Host 'Insufficient credentials, or an invalid interface was provided.  Please try again.'
+                Write-Host "Insufficient credentials, or an invalid interface was provided.  Please try again."
                 BREAK
             } #If
         } # If
-        if ($ShowSteps) {Write-Host "Testing connection to $Address..."} #If
 
         while ($Difference -gt 1) {
-        $Numberofsteps ++
 
-            if ($Showsteps) {
-            Write-Host "Trying Size: $MTUSize
-            "
-            Write-Host "Incrementing steps: $Numberofsteps"
-            } #If
-
-        & 'PING.EXE' -f $Address -l $MTUSize -n 1 | Out-Null
-
-    
-            if ($LASTEXITCODE -eq 0) {
-                if ($ShowSteps) {
-                    Write-Host "Splitting difference: $Difference
-                    Difference: $Difference"
-                } #If
-
+            Write-Verbose "Trying Size: $MTUSize"
+            $TestPing = $PingObject.Send($Address,$Timeout,$($ByteBuffer[1..$MTUSize]),$PingOptions)
+            $Ping     = $PingObject.Send($Address,$Timeout,$($ByteBuffer[1]))
+            $Difference = [math]::Ceiling($Difference / 2 )
+            if ($TestPing.status.ToString() -eq 'Success') {
+                Write-Verbose "Success"
                 $MTUSize += $Difference
-                $Difference = [math]::Truncate($Difference / 2 )        
 
+            } else {
+                if ($Ping.Status.ToString() -eq 'Success') {
+                Write-Verbose "Failure"
+                $MTUSize -= $Difference
                 } else {
-                    if ($ShowSteps) {
-                        Write-Host "Splitting difference: $Difference"
-                    } #If
-
-                    $MTUSize -= $Difference
-                    $Difference = [math]::Truncate($Difference / 2 )
-
+                Write-Verbose "Suspect packet drop, trying again."
+                $Difference = [math]::Ceiling($Difference * 2 )
                 } #If/Else
+            } #If/Else
     
         } #While
 
         while ($Difference -eq 1 -and $Count -ne 2) {
 
-        & PING.EXE -f $Address -l $MTUSize -n 1 | Out-Null
-
-        $Numberofsteps ++
-            if ($ShowSteps) {
-                Write-Host "Incrementing steps: $Numberofsteps"
-            } #If
-
-            if ($LASTEXITCODE -eq 0) {
+        $TestPing = $PingObject.Send($Address,$Timeout,$($ByteBuffer[1..$MTUSize]),$PingOptions)
                 
+            if ($TestPing.status.ToString() -eq 'Success') {
             $MTUSize ++
             $Count ++
-                if ($ShowSteps) {
-                    Write-Host "Incrementing MTUSize: $MTUSize
-                    Incrementing Count: $Count"
-                } #If
+            Write-Verbose "Incrementing MTUSize: $MTUSize"
 
             } else { 
 
             $MTUSize -= 2 
             $Count = 0
-                if ($ShowSteps) {
-                Write-Host "Failure, Count=$Count
-                Incrementing MTUSize: $MTUSize"
-                
-                } #If
+            Write-Verbose "Failure, Count=$Count `nIncrementing MTUSize: $MTUSize"
+
             } #If/Else
 
         } #While
 
         $MTUSize -= 1
-
-        if ($ShowSteps) {
-            Write-Host "Incrementing MTUSize: $MTUSize"
-        } #If
-
-        $24MTUSize = $MTUSize + 24  # Add 24 bits for frame header that is not included in ping size (payload) parameters.  (Payload+Header=MTU)
-        $28MTUSize = $MTUSize + 28  # If the computer uses a VLAN, add 4 bits to frame header.
-            Write-Host "
-        
- Optimal payload size of $MTUSize was found in $Numberofsteps steps for the address: $Address
- The MTU size below should be <= $28MTUSize" 
+        Write-Verbose "Incrementing MTUSize: $MTUSize"
+        $24MTUSize = $MTUSize + 24  # Add 24 bytes for packet header that is not included in ping size (payload) parameters.  (Payload+Header=MTU)
+        $28MTUSize = $MTUSize + 28  # If the computer uses a VLAN, add 4 bytes for packet header.
+            Write-Verbose "`n`n Optimal payload size of $MTUSize was found for the address: $Address `n The MTU size below should be <= $28MTUSize, depending on local VLAN options `n" 
 
         # Change internet adapter MTU settings.
         
         if ($Set) {
 
-            if ($ShowSteps) {
-                        Write-Host "Setting Local interface to: $24MTUSize to account for frame header"
-            } #If
-        & netsh.exe interface ipv4 set interface "$Interface" "mtu=$24MTUSize" | Out-Null
-        
-        & PING.EXE -f $Address -l $MTUSize -n 1 | out-null
-
-            if ($LASTEXITCODE -ne 0) {
-                if ($ShowSteps) {
-                    Write-Host "VLAN detected, setting MTU to: $28MTUSize"
-                } #If
-            & netsh.exe interface ipv4 set interface "$Interface" "mtu=$28MTUSize" | Out-Null
-            } #If
-        Write-Host '
-        Current Configuration:
-        '
-        & netsh.exe interface ipv4 show interfaces
+            Write-Verbose "Setting Local interface to: $24MTUSize to account for packet header"
+            & netsh.exe interface ipv4 set interface "$Interface" "mtu=$24MTUSize" | Out-Null
+        Write-Verbose "Testing with interface MTU set to detect VLAN settings."
+        $TestPing = $PingObject.Send($Address,$Timeout,$($ByteBuffer[1..$MTUSize]),$PingOptions)
+              
+            if ($TestPing.status.ToString() -ne 'Success') {
+                Write-Verbose "VLAN detected, setting MTU to: $28MTUSize"
+                & netsh.exe interface ipv4 set interface "$Interface" "mtu=$28MTUSize" | Out-Null
+                return ($24MTUSize)
+            } else {
+                Write-Verbose "VLAN not detected"
+                return ($28MTUSize)
+            }#If
 
 
         } else {
-        Write-Host 'This number is based on your current settings below:'
-        & netsh.exe interface ipv4 show interfaces
+        return ($28MTUSize)
         } #If/Else
     } #Process
-
     END {}
-
 } #Function
