f9107e94f11165fadd99c117aa12bcfe89442c9b
Taylor Whitt <Taylor.Whitt@gmail.com>
2016-07-19 13:55:07 -0700

    Update Get-MTU.ps1
    Applied fix to surprise bug where if MaxMTU was set below Interface capability and -Set was on, it would continue past the value due to internal fragmentation which resulted in the DF bit being applied to fragmented packets.
    
    Changed "While" statement to reflect conditions the script is looking for.  Shortened script ~20 lines.
    Added $LastLatency variable to reduce timeout of failed verification/retry.

--- Get-MTU.ps1
+++ Get-MTU.ps1
@@ -12,23 +12,24 @@ Function Get-MTU {
     -Set 
     Will perform netsh commands and prompt the user to change the interface specified to optimal settings.
     *Requires elevated privledges*
-    -ShowSteps 
-    Will write messages to the console to walk the user through the steps being performed within the function.
     -Idx
     Identifies the local interface to change within the -Set switch. If left blank, you will be prompted with your options.
+    -MaxMTU
+    Limits the highest number of bytes in the packet to test for (default 35840)
+    -Timeout
+    Changes the timeout parameter in miliseconds of the ping packet (default 500)
 	
 	.Example
-	 Find-MTU www.google.com
+	 Get-MTU www.google.com
         Will conduct a series of Do-Not-Fragment ping requests to "www.google.com" until a Maximum Transmission Unit is found, and output 
-the result to the host. This does not account for the additional frame header which is an additional 24 or 28 bits depending on VLAN settings.
+the result to the host.  If the value is that of the interface used, it may not represent the maximum value.
      .Example
-     Find-MTU 8.8.8.8 -Set -ShowSteps
-Will conduct a series of Do-Not-Fragment ping requests target 8.8.8.8 until a Maximum Transmission Unit is found.
+     Get-MTU 8.8.8.8 -Set
+After prompting which interface (Idx) will be set and changes the max value to $MaxMTU, will conduct a series of Do-Not-Fragment ping requests target 8.8.8.8 until a Maximum Transmission Unit is found.
 Once found, will promt the user to select which interface is being used and will change the interface's MTU settings accordingly.
 This will account for the additional frame header and detects VLAN settings. Each step will be written to the console.
 **NOTE** "-Set" requires administrator privledges to implement network interface changes.
 #>
-    
     Param(
         [Parameter(Mandatory=$True,
                    Position=1,
@@ -49,49 +50,54 @@ This will account for the additional frame header and detects VLAN settings. Eac
     BEGIN {
     $PingObject = New-Object System.Net.NetworkInformation.Ping
     $PingOptions = New-Object System.Net.NetworkInformation.PingOptions
-    [byte[]]$ByteBuffer = ,0xAC * $MaxMTU
+    $MaxMTU = [math]::Abs($MaxMTU)
+    [byte[]]$ByteBuffer = ,0xff * $MaxMTU
     [void][int]$MTUSize
     [void][int]$Difference 
-    [int]$Difference = [math]::Abs($MaxMTU)
+    [int]$Difference = $MaxMTU
     }
 
     PROCESS {
+
         Write-Verbose "Testing connection to $Address..."
         $TestPing = $PingObject.Send($Address,$Timeout,$($ByteBuffer[1]))
         if ($TestPing.status.ToString() -ne 'Success') {
-            $TestPing.status.ToString()
-            Write-Host "The address specified was not reachable. Please provide a reachable hostname or IP address, or try again. `nYou Provided: $Address"
-            Break
+            Write-Verbose "The address specified was not reachable. Please provide a reachable hostname or IP address, or try again. `nYou Provided: $Address"
+            return {Target Unreachable}
         } #If
 
         if ($Set) {
-
+            #Temporary fix to odd bug where if MaxMTU was set below Interface capability, it would continue past the value due to internal fragmentation.
+            $MaxMTU += 28
             if ($Idx -ne $Null) {
                 $Interface = $Idx
             } else {
-                & netsh.exe interface ipv4 show interfaces |Out-Null
+                & netsh.exe interface ipv4 show interfaces | Out-Null
                 $Interface = Read-Host 'Input which interface (Idx number) you would like to change'
             }
-
-        & netsh.exe interface ipv4 set interface "interface=$Interface" "mtu=$MaxMTU"
+            
+            & netsh.exe interface ipv4 set interface "interface=$Interface" "mtu=$MaxMTU" | Out-Null
             if ($LASTEXITCODE -ne 0) {
-                Write-Host "Insufficient credentials, or an invalid interface was provided.  Please try again."
-                BREAK
+                Write-Error "Insufficient credentials, or an invalid interface was provided.  Please try again.  Continuing to find MTU with current settings..."
+                $Set = $Null
             } #If
         } # If
-
-        while ($Difference -gt 1) {
-
+        $MTUSize = 0
+        $DifferenceCount = 0
+        $LastLatency = $Timeout
+        while (($Difference -gt 1 -or $TestPing.Status.Tostring() -eq 'Success' -or $DifferenceCount -le 1) -and $MTUSize -lt ($MaxMTU - 24)) {
+        
             Write-Verbose "Trying Size: $MTUSize"
             $TestPing = $PingObject.Send($Address,$Timeout,$($ByteBuffer[1..$MTUSize]),$PingOptions)
-            $Ping     = $PingObject.Send($Address,$Timeout,$($ByteBuffer[1]))
-            $Difference = [math]::Ceiling($Difference / 2 )
+            if ($Difference -eq 1) { $DifferenceCount += 1 
+            } else { $Difference = [math]::Ceiling($Difference / 2 ) }
             if ($TestPing.status.ToString() -eq 'Success') {
                 Write-Verbose "Success"
                 $MTUSize += $Difference
-
+                $LastLatency = ($TestPing.RoundtripTime + 15)
             } else {
-                if ($Ping.Status.ToString() -eq 'Success') {
+                $TestPing = $PingObject.Send($Address,$LastLatency,$($ByteBuffer[1..$MTUSize]),$PingOptions)
+                if ($TestPing.Status.ToString() -ne 'Success') {
                 Write-Verbose "Failure"
                 $MTUSize -= $Difference
                 } else {
@@ -99,30 +105,9 @@ This will account for the additional frame header and detects VLAN settings. Eac
                 $Difference = [math]::Ceiling($Difference * 2 )
                 } #If/Else
             } #If/Else
-    
-        } #While
-
-        while ($Difference -eq 1 -and $Count -ne 2) {
-
-        $TestPing = $PingObject.Send($Address,$Timeout,$($ByteBuffer[1..$MTUSize]),$PingOptions)
-                
-            if ($TestPing.status.ToString() -eq 'Success') {
-            $MTUSize ++
-            $Count ++
-            Write-Verbose "Incrementing MTUSize: $MTUSize"
-
-            } else { 
-
-            $MTUSize -= 2 
-            $Count = 0
-            Write-Verbose "Failure, Count=$Count `nIncrementing MTUSize: $MTUSize"
-
-            } #If/Else
 
         } #While
 
-        $MTUSize -= 1
-        Write-Verbose "Incrementing MTUSize: $MTUSize"
         $24MTUSize = $MTUSize + 24  # Add 24 bytes for packet header that is not included in ping size (payload) parameters.  (Payload+Header=MTU)
         $28MTUSize = $MTUSize + 28  # If the computer uses a VLAN, add 4 bytes for packet header.
             Write-Verbose "`n`n Optimal payload size of $MTUSize was found for the address: $Address `n The MTU size below should be <= $28MTUSize, depending on local VLAN options `n" 
@@ -139,10 +124,15 @@ This will account for the additional frame header and detects VLAN settings. Eac
             if ($TestPing.status.ToString() -ne 'Success') {
                 Write-Verbose "VLAN detected, setting MTU to: $28MTUSize"
                 & netsh.exe interface ipv4 set interface "$Interface" "mtu=$28MTUSize" | Out-Null
-                return ($24MTUSize)
+                $TestPing = $PingObject.Send($Address,$Timeout,$($ByteBuffer[1..$MTUSize]),$PingOptions)
+                if ($TestPing.status.ToString() -ne 'Success') {
+                Write-error "Something went wrong"
+                } else {
+                return ($28MTUSize)
+                }
             } else {
                 Write-Verbose "VLAN not detected"
-                return ($28MTUSize)
+                return ($24MTUSize)
             }#If
 
 
